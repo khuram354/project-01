7b0a687d84a3e46fde1eed7383f37359c65c99ec
Khuram Shahzad <khurammirza354@gmail.com>
2023-09-23 14:04:45 +0500

    section-1 complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let list = document.getElementById("list");
 let menu = document.getElementById("menu");
 
 menu.addEventListener("click", () => {
-    if (list.style.display === "none" || list.style.display === "") {
+    if (list.style.display == "none" || list.style.display == "") {
         list.style.display = "block";
     } else {
         list.style.display = "none";
